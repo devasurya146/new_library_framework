066c0fb71ef34855de4c628b7e0ee54e8c0144f4
Dinesh <106303826+devasurya146@users.noreply.github.com>
2023-03-22 15:11:16 +0530

    Initial Commit

---
+++ newlibrary/newlibrary.h
@@ -0,0 +1,18 @@
+//
+//  newlibrary.h
+//  newlibrary
+//
+//  Created by Dinesh on 22/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for newlibrary.
+FOUNDATION_EXPORT double newlibraryVersionNumber;
+
+//! Project version string for newlibrary.
+FOUNDATION_EXPORT const unsigned char newlibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <newlibrary/PublicHeader.h>
+
+
